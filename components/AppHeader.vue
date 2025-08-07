028c676b1d5abb8355b75d35cd8a1c9f9ba32365
Iain <iain@humblyarrogant.io>
2025-08-07 16:59:58 +0200

    Update navbar styling: center logo and menu with black background and white text

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,10 +1,10 @@
 <template>
-  <header class="sticky top-0 w-full bg-black text-white py-4 shadow-md z-50">
-    <div class="container mx-auto px-4 flex justify-between items-center">
+  <header class="sticky top-0 w-full bg-black text-white py-6 shadow-md z-50">
+    <div class="container mx-auto px-4 flex flex-col items-center space-y-4">
       <NuxtLink to="/" class="text-yellow-400 font-bold text-xl">Dev-Speak</NuxtLink>
       
       <nav>
-        <ul class="flex space-x-6">
+        <ul class="flex space-x-6 justify-center">
           <li><NuxtLink to="#" class="hover:text-yellow-400 transition-colors">Blog</NuxtLink></li>
           <li><NuxtLink to="#" class="hover:text-yellow-400 transition-colors">Buy Book</NuxtLink></li>
           <li><NuxtLink to="#" class="hover:text-yellow-400 transition-colors">About</NuxtLink></li>
