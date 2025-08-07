9b53f3973c5fcb80e510ccbde266f50b8dfed4d6
Iain Cambridge <iain@tandem.net>
2025-08-07 10:53:16 +0200

    Create index page with centered 'hello world' text

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello-container">
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Using Composition API with script setup as per guidelines
+import { defineComponent } from 'vue';
+
+// Define a multi-word component name to satisfy ESLint rule
+defineComponent({
+  name: 'IndexPage'
+});
+
+definePageMeta({
+  name: 'HomePage'
+});
+</script>
+
+<style scoped>
+.hello-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+}
+
+h1 {
+  font-size: 2rem;
+}
+</style>
