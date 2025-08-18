03d58d23a11c6383350403df08e64884bcda255a
Iain <iain@humblyarrogant.io>
2025-08-18 23:23:30 +0200

    Add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  app: {
+    head: {
+      script: [{src: 'https://stats.iain.rocks/info.js', async: true, defer: 'true', referrerpolicy: 'unsafe-url', 'data-domain': 'dev-speak.com'}],
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
